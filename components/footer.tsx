b5a5941d9f9342928c6b6232217e54f5da866042
deadpine <hey@deadpine.xyz>
2025-06-01 23:04:28 -0300

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,12 +1,10 @@
 export function Footer() {
   return (
-    <footer className="bg-[#130102] text-white py-12 px-8">
+    <footer className="bg-[#130102] text-white pt-4 pb-8 px-8">
       <div className="max-w-7xl mx-auto">
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
-          <img src="/images/vernissage-logo.png" alt="Vernissage" className="h-16 my-8" />
-        </div>
-        <div className="border-t border-[#2E211F] mt-8 pt-8 text-center text-[#53402D] text-lg font-serif flex flex-col md:flex-row justify-between">
-          <p>hola@vernissageteatro.com</p>
+        <img src="/images/vernissage-logo.png" alt="Vernissage logo" className="h-16 my-16" />
+        <div className="border-t border-[#2E211F] pt-8 text-center text-[#53402D] text-lg font-serif flex flex-col md:flex-row justify-between">
+          <a href="mailto:hola@vernissageteatro.com" className="hover:text-[#A17345]">hola@vernissageteatro.com</a>
           <p>© {new Date().getFullYear()} Vernissage. Todos los secretos reservados.</p>
           <p>diseño por <a href="https://deadpine.xyz" target="_blank" rel="noopener noreferrer" className="italic hover:text-[#A17345]">deadpine.xyz</a></p>
         </div>
