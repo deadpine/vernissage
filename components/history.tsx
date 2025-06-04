01db0a4140fdb0dd87f597e2cba22fb0ba99e582
deadpine <hey@deadpine.xyz>
2025-06-04 11:35:39 -0300

    new story text

--- components/history.tsx
+++ components/history.tsx
@@ -3,18 +3,20 @@ export function HistorySection() {
     <div className="bg-[#171614] text-white border-b-4 border-gold">
       
       <div className="bg-[url('/images/history-bg.png')] bg-cover md:bg-contain bg-center bg-no-repeat 
-      min-h-[400px] py-[150px] md:py-[134px] relative">
+      min-h-[400px] py-[126px] md:py-[134px] md:px-12 relative">
         
         {/* papel */}
-        <div className="max-w-[calc(100%-2rem)] md:max-w-[476px] space-y-6 mx-auto text-center px-6 py-8 md:p-9 border border-[#725337]/60 rotate-[-1deg] md:rotate-[-2deg] relative">
-          <h2 className="text-4xl font-heading text-[#725337] mb-8">HISTORIA</h2>
-          <p className="text-[#4B3420] font-serif text-lg">
-            Durante la restauración de una imponente mansión en Punta del Este, los nuevos propietarios encuentran escondidos los rastros de un artista que aparentemente vivió y creó dentro de la casa: óleos, esculturas, cartas, fotos con otros íconos artísticos de la época, maquetas, audios y material fílmico. Una obra tan potente como desconocida hasta el día de hoy. Y un enigma. ¿Quién era este artista? ¿Por qué desapareció sin dejar rastro? ¿Qué intentaba decirnos con su obra?
-          </p>
-          <p className="text-[#4B3420] font-serif text-lg mb-2">
-            Para compartir el hallazgo, los nuevos dueños de la propiedad organizan una Vernissage inspirada en el mundo bohemio y extravagante en el que vivió este artista en los años 60. 
-            Un grupo reducido y selecto de invitados se sumergirá en una experiencia donde las obras, los espacios y hasta los objetos parecen hablar. Las habitaciones se transforman en escenarios vivos. La casa revela secretos, y la sola presencia de cada visitante activa piezas de una narrativa que no termina de explicarse del todo.
-          </p>
+        <div className="flex flex-col items-center max-w-[calc(100%-1.5rem)] md:max-w-[450px] md:space-y-10 space-y-6 mx-auto text-center mb-1 px-6 py-8 md:p-9 border border-[#725337]/60 rotate-[-1deg] md:rotate-[-2deg] relative">
+          <h2 className="text-4xl font-heading text-[#725337]">HISTORIA</h2>
+          <div>
+            <p className="text-[#4B3420] font-serif text-lg mb-6">
+              Durante la restauración de una imponente mansión en Punta del Este, los nuevos propietarios descubren rastros de un artista que aparentemente vivió y creó en la casa. Óleos, esculturas, cartas, fotos y material fílmico. Una obra tan potente como desconocida. Y un enigma. ¿Quién era ese artista? ¿Por qué desapareció sin dejar rastro? ¿Qué intentaba decirnos con su obra?
+            </p>
+            <p className="text-[#4B3420] font-serif text-lg mb-2">
+              Para compartir el hallazgo, organizan una vernissage íntima e inmersiva, inspirada en el universo onírico y excéntrico que rodeaba al artista. Un grupo selecto de invitados se sumergirá en una experiencia donde la casa cobra vida. Los espacios, los objetos y las obras parecen hablar, y cada visitante se convierte en parte del misterio.
+            </p>
+          </div>
+          <img src="/images/cerradura.png" alt="" className="hidden md:block" />
         </div>
 
         <img src="/images/history-left.png" alt="History" className="absolute left-0 top-0 h-full w-auto object-contain hidden lg:block z-10" />
