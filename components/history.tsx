6b65266c89b4dd4f44610f535a750e1791aabbfe
deadpine <hey@deadpine.xyz>
2025-06-04 01:59:10 -0300

    Update history.tsx

--- components/history.tsx
+++ components/history.tsx
@@ -2,10 +2,11 @@ export function HistorySection() {
   return (
     <div className="bg-[#171614] text-white border-b-4 border-gold">
       
-      <div className="bg-[url('/images/history-bg.png')] bg-cover md:bg-contain bg-center bg-no-repeat min-h-[400px] py-[138px] md:py-[134px] relative">
+      <div className="bg-[url('/images/history-bg.png')] bg-cover md:bg-contain bg-center bg-no-repeat 
+      min-h-[400px] py-[150px] md:py-[134px] relative">
         
         {/* papel */}
-        <div className="max-w-[calc(100%-2rem)] md:max-w-[476px] space-y-6 mx-auto text-center px-7 py-8 md:p-9 border border-[#725337]/60 rotate-[-1deg] md:rotate-[-2deg] relative">
+        <div className="max-w-[calc(100%-2rem)] md:max-w-[476px] space-y-6 mx-auto text-center px-6 py-8 md:p-9 border border-[#725337]/60 rotate-[-1deg] md:rotate-[-2deg] relative">
           <h2 className="text-4xl font-heading text-[#725337] mb-8">HISTORIA</h2>
           <p className="text-[#4B3420] font-serif text-lg">
             Durante la restauración de una imponente mansión en Punta del Este, los nuevos propietarios encuentran escondidos los rastros de un artista que aparentemente vivió y creó dentro de la casa: óleos, esculturas, cartas, fotos con otros íconos artísticos de la época, maquetas, audios y material fílmico. Una obra tan potente como desconocida hasta el día de hoy. Y un enigma. ¿Quién era este artista? ¿Por qué desapareció sin dejar rastro? ¿Qué intentaba decirnos con su obra?
