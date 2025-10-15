56760464e17449f771dbc6f98036754e179f8496
deadpine <hey@deadpine.xyz>
2025-10-15 16:20:34 -0300

    agregar estreno

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ export default function Page() {
         <AboutSection />
         <ExperienceSection />
         <HistorySection />
-        <TiersSection />
+        {/* <TiersSection /> */}
         <FAQSection />
       </div>
       <Footer />
