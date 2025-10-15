6a6793087998672ad6ffafe3708bcdf3c76f5b0a
deadpine <hey@deadpine.xyz>
2025-10-15 16:29:35 -0300

    Update history.tsx

--- components/history.tsx
+++ components/history.tsx
@@ -6,14 +6,18 @@ export function HistorySection() {
       min-h-[400px] py-[126px] md:py-[134px] md:px-12 relative">
         
         {/* papel */}
-        <div className="flex flex-col items-center max-w-[calc(100%-1.5rem)] md:max-w-[450px] md:space-y-10 space-y-6 mx-auto text-center mb-1 px-6 py-8 md:p-9 border border-[#725337]/60 rotate-[-1deg] md:rotate-[-2deg] relative">
+        <div className="flex flex-col items-center max-w-[calc(100%-1.5rem)] md:max-w-[480px] md:space-y-10 space-y-6 mx-auto text-center mb-1 px-6 py-8 md:p-9 border border-[#725337]/60 rotate-[-1deg] md:rotate-[-2deg] relative">
           <h2 className="text-4xl font-heading text-[#725337]">HISTORIA</h2>
           <div>
             <p className="text-[#4B3420] font-serif text-lg mb-6">
-              Durante la restauración de una imponente mansión en Punta del Este, los nuevos propietarios descubren rastros de un artista que aparentemente vivió y creó en la casa. Óleos, esculturas, cartas, fotos y material fílmico. Una obra tan potente como desconocida. Y un enigma. ¿Quién era ese artista? ¿Por qué desapareció sin dejar rastro? ¿Qué intentaba decirnos con su obra?
+              Durante la restauración de la mítica Residencia Sazsh, una mansión frente al mar en Punta del Este, Inés —sobrina y heredera del artista Bernardo Sazsh, emblema del arte uruguayo de los 80 y principios de los 90— descubre un cuarto cerrado desde hace décadas. Allí se encuentran obras y objetos que se creían perdidos tras el incendio que destruyó el atelier del artista.
+            </p>
+            <p className="text-[#4B3420] font-serif text-lg mb-6">
+              Para celebrar el hallazgo, organiza una vernissage que revive las míticas Veladas Sazsh: noches de arte, delicias, música y secretos.
+              Pero la casa parece guardar su propia memoria… y los recuerdos comienzan a cobrar vida.
             </p>
             <p className="text-[#4B3420] font-serif text-lg mb-2">
-              Para compartir el hallazgo, organizan una vernissage íntima e inmersiva, inspirada en el universo onírico y excéntrico que rodeaba al artista. Un grupo selecto de invitados se sumergirá en una experiencia donde la casa cobra vida. Los espacios, los objetos y las obras parecen hablar, y cada visitante se convierte en parte del misterio.
+            Vernissage es una experiencia teatral inmersiva en la que los invitados —un grupo reducido y selecto de espectadores— atraviesan una casa que respira, escucha y habla.
             </p>
           </div>
           <img src="/images/cerradura.png" alt="" className="hidden md:block" />
